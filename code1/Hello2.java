12130c78f01f786b656f0eb57141ddf07290b622
SanguineSkral <sanguineskralx@gmail.com>
2016-10-25 12:58:27 -0700

    Add files via upload

---
+++ code1/Hello2.java
@@ -0,0 +1,8 @@
+// save this file in a folder called java uou have create in home
+// Hello2.java
+
+class Hello2 {
+	public static void main (String a[]){
+		System.out.print("Hello 2 world!");
+	}
+}
---
+++ code1/README.md
@@ -0,0 +1,2 @@
+# Java
+Snailthulhu
