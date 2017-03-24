c631075dc526979c12d9b624da65838778dbe2cb
SanguineSkral <sanguineskralx@gmail.com>
2017-03-24 13:46:44 -0700

    Cleaned it up a bit

--- Hello2.java
+++
@@ -1,8 +0,0 @@
-// save this file in a folder called java uou have create in home
-// Hello2.java
-
-class Hello2 {
-	public static void main (String a[]){
-		System.out.print("Hello 2 world!");
-	}
-}
